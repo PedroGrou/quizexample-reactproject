03f1b9cf59817597c92de3d15ade3e324df5b81e
PedroGrou <pedrogroug@gmail.com>
2021-07-25 12:30:52 +0100

    first commit

---
+++ src/SetupForm.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useGlobalContext } from './context';
+
+const SetupForm = () => {
+  const { quiz, handleChange, handleSubmit, error } = useGlobalContext();
+
+  return (
+    <main>
+      <section className="quiz quiz-small">
+        <form className="setup-form">
+          <div className="form-control">
+            <h2>Setup Quiz</h2>
+            <label htmlFor="amount">number of questions</label>
+            <input
+              type="number"
+              name="amount"
+              id="amount"
+              value={quiz.amount}
+              onChange={handleChange}
+              className="form-input"
+              min={1}
+              max={50}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="category">Select Category</label>
+            <select name="category" id="category" className="form-input" value={quiz.category} onChange={handleChange}>
+              <option value="sports">Sports</option>
+              <option value="history">History</option>
+              <option value="politics">Politics</option>
+            </select>
+          </div>
+          <div className="form-control">
+            <label htmlFor="difficulty">Select Difficulty</label>
+            <select
+              name="difficulty"
+              id="difficulty"
+              className="form-input"
+              value={quiz.difficulty}
+              onChange={handleChange}
+            >
+              <option value="easy">Easy</option>
+              <option value="medium">Medium</option>
+              <option value="hard">Hard</option>
+            </select>
+          </div>
+          {error && <p className="error">can't generate questions, please try different options</p>}
+          <button type="submit" onClick={handleSubmit} className="submit-btn">
+            start
+          </button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default SetupForm;
